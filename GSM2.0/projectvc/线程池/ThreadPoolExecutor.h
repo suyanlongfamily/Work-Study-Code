fc6bfcae6cb1f3e093969417d6a122941df545b2
suyanlong <5929238410@qq.com>
2017-02-21 14:04:39 +0800

    线程池学习

--- GSM2.0/projectvc/线程池/ThreadPoolExecutor.h
+++ GSM2.0/projectvc/线程池/ThreadPoolExecutor.h
@@ -1,4 +1,4 @@
-#pragma once
+pragma once
 #ifndef __THREAD_POOL_EXECUTOR__  
 #define __THREAD_POOL_EXECUTOR__  
   
--- GSM2.0/projectvc/线程池/线程池.cpp
+++ GSM2.0/projectvc/线程池/线程池.cpp
@@ -25,14 +25,43 @@ protected:
 private:
 };
 
+
+void DealDataFun(
+	__out LPDWORD lpNumberOfBytesTransferred,
+	__out PULONG_PTR lpCompletionKey,
+	__out LPOVERLAPPED *lpOverlapped
+	)
+{
+
+	//处理xnetd发送过来数据的主要线程。即主处理类
+	int* p = (int*)lpCompletionKey;
+
+	while (*lpCompletionKey)
+	{
+		cout<<"----"<<*lpCompletionKey<<endl;
+		(*lpCompletionKey)--;	
+	} 
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-	ThreadPool tp;
-	for (int i=0;i<200;i++)
-	{
-		tp.Call(fun,0);
-	}
-	Test t;
+	//ThreadPool tp;
+	//for (int i=0;i<200;i++)
+	//{
+	//	tp.Call(fun,0);
+	//}
+	//Test t;
+
+	FThreadPool* threadPool = FThreadPool::GetTreadPool();
+
+	threadPool->StartTreadPool(DealDataFun,NULL);
+	
+
+	int* p = new int(900);
+	threadPool->PostTaskToQueue(0,(ULONG_PTR)p);
+
+
+	//threadPool.StartTreadPool();
 
 	std::system("pause");
 	return 0;
